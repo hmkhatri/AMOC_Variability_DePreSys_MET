58c738f5fd10df346384c25c2c6ae464aa48651a
Hemant Khatri <hemantkhatri1091@gmail.com>
2022-01-26 16:22:42 +0000

    dask multi-core runs and NAO-psi correlation analysis

--- Python_Scripts/Correlation_NAO_ocean_var.py
+++ Python_Scripts/Correlation_NAO_ocean_var.py
@@ -11,111 +11,125 @@ import xarray as xr
 from dask.distributed import Client, LocalCluster
 from dask import delayed
 from dask import compute
+from dask.diagnostics import ProgressBar
 
-from dask_mpi import initialize
+#from dask_mpi import initialize
 
 #initialize()
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
      
-    initialize()
-    client = Client()
-    print(client)
+    #initialize()
+    #client = Client()
+    #print(client)
     
-    # ------- Function for correlations -----
-    def corr_NAO(NAO_ind, ds, dim='time'):
+# ------- Functions -----
+def corr_NAO(NAO_ind, ds, dim='time'):
 
-	ds_season = ds.resample(time='QS-DEC').mean('time')
-        corr = xr.corr(NAO_ind, ds_season, dim=dim)
+    #ds_season = ds.resample(time='QS-DEC').mean('time')
+    corr = xr.corr(NAO_ind, ds, dim=dim)
 
-        return corr
-
-
-    # -------- Define paths and read data --------------
-
-    ppdir="/badc/cmip6/data/CMIP6/DCPP/MOHC/HadGEM3-GC31-MM/dcppA-hindcast/"
-
-    save_path="/home/users/hkhatri/DePreSys4_Data/Data_Anomaly_Compute/"
-
-    ppdir_drift="/home/users/hkhatri/DePreSys4_Data/Data_Drift_Removal/Drift_1970_2016_Method_DCPP/"
-
-    year1, year2 = (1960, 2017)
-
-    var_list = ['tos', 'sos'] #, 'hfds'
-
-    # NAO data and create seasonal data
-    ds_NAO = xr.open_dataset(save_path + "NAO_SLP_Anomaly.nc")
-
-    NAO = ds_NAO['NAO'].copy()
-    tim = ds_NAO['time_val'].isel(start_year=0).drop('start_year')
-    NAO = NAO.assign_coords(time=tim)
-
-    NAO = NAO.isel(time=slice(1,len(NAO.time)-1)) # get rid of first Nov and last Mar for better seasonal avg
-    NAO = NAO.resample(time='QS-DEC').mean('time')
+    return corr
     
-    for var in var_list:
+def resample_season(ds):
+        
+    ds_season = ds.resample(time='QS-DEC').mean('time')
+        
+    return ds_season
 
-        # Read drift data
-        ds_drift = []
 
-        for r in range (0,10):
+# -------- Define paths and read data --------------
 
-            ds1 = []
-            for lead_year in range(0,11):
+ppdir="/badc/cmip6/data/CMIP6/DCPP/MOHC/HadGEM3-GC31-MM/dcppA-hindcast/"
 
-                d = xr.open_dataset(ppdir_drift + var + "/Drift_"+ var + "_r" + 
-                                    str(r+1) +"_Lead_Year_" + str(lead_year+1) + ".nc")
-                d = d.assign(time = np.arange(lead_year*12, 12*lead_year + np.minimum(12, len(d['time'])), 1))
-                ds1.append(d)
+ppdir_NAO="/home/users/hkhatri/DePreSys4_Data/Data_Anomaly_Compute/"
+
+ppdir_drift="/home/users/hkhatri/DePreSys4_Data/Data_Drift_Removal/Drift_1970_2016_Method_DCPP/"
+    
+save_path="/home/users/hkhatri/DePreSys4_Data/Data_Correlation/"
+
+year1, year2 = (1960, 2017)
+
+var_list = ['mlotst'] #'tos', 'sos', 'hfds'
+
+# NAO data and create seasonal data
+ds_NAO = xr.open_dataset(ppdir_NAO + "NAO_SLP_Anomaly.nc")
+
+NAO = ds_NAO['NAO']
+tim = ds_NAO['time_val'].isel(start_year=0).drop('start_year')
+NAO = NAO.assign_coords(time=tim)
+NAO = NAO.chunk({'start_year':-1, 'r':1, 'time':1})
+
+NAO = NAO.isel(time=slice(1,len(NAO.time)-1)) # get rid of first Nov and last Mar for better seasonal avg
+NAO = NAO.resample(time='QS-DEC').mean('time')
+    
+for var in var_list:
+
+    # Read drift data
+    ds_drift = []
+
+    for r in range (0,10):
+
+        ds1 = []
+        for lead_year in range(0,11):
+
+            d = xr.open_dataset(ppdir_drift + var + "/Drift_"+ var + "_r" + 
+                                str(r+1) +"_Lead_Year_" + str(lead_year+1) + ".nc")
+            d = d.assign(time = np.arange(lead_year*12, 12*lead_year + np.minimum(12, len(d['time'])), 1))
+            ds1.append(d)
                 
-            ds1 = xr.concat(ds1, dim='time')
-            ds_drift.append(ds1)
+        ds1 = xr.concat(ds1, dim='time')
+        ds_drift.append(ds1)
 
-        ds_drift = xr.concat(ds_drift, dim='r')
-        ds_drift = ds_drift.drop('time')
-        ds_drift = ds_drift.chunk({'r':-1, 'time':-1, 'j':10, 'i':10})
+    ds_drift = xr.concat(ds_drift, dim='r')
+    ds_drift = ds_drift.drop('time')
+    ds_drift = ds_drift.chunk({'r':1, 'time':1, 'j':50, 'i':50})
 
-        print("Drift Data read complete")
+    print("Drift Data read complete")
 
-        # Read full data
-        for r in range(0,10):
+    # Read full data
+    for r in range(2,10):
 
-            print("Var = ", var, "; Ensemble = ", r)
+        print("Var = ", var, "; Ensemble = ", r)
 
-            ds = []
+        ds = []
 
-            for year in range(year1, year2, 1):
+        for year in range(year1, year2, 1):
 
-                var_path = "s" + str(year) +"-r" + str(r+1) + "i1p1f2/Omon/" + var + "/gn/files/d20200417/"
+            var_path = "s" + str(year) +"-r" + str(r+1) + "i1p1f2/Omon/" + var + "/gn/latest/"
 
-                d = xr.open_mfdataset(ppdir + var_path + "*.nc")
-                d = d.drop(['time', 'vertices_latitude', 'vertices_longitude', 'time_bnds'])
+            d = xr.open_mfdataset(ppdir + var_path + "*.nc")
+            d = d.drop(['time', 'vertices_latitude', 'vertices_longitude', 'time_bnds'])
 
-                ds.append(d)
+            ds.append(d)
 
-            # combine data for hindcasts
-            ds = xr.concat(ds, dim='start_year')
-            ds = ds.isel(i=slice(749,1199), j = slice(699, 1149))
-            ds = ds.chunk({'start_year':-1, 'time':-1, 'j':10, 'i':10})
-            print("Data read complete")
+        # combine data for hindcasts
+        ds = xr.concat(ds, dim='start_year')
+        ds = ds.isel(i=slice(749,1199), j = slice(699, 1149))
+        ds = ds.chunk({'start_year':-1, 'time':1, 'j':50, 'i':50})
+        print("Data read complete")
 
-            # seasonal means and correlations
-            tmp_var = ds[var] - ds_drift[var].isel(r=r)
+        # seasonal means and correlations
+        tmp_var = ds[var] - ds_drift[var].isel(r=r)
 
-            tmp_var = tmp_var.assign_coords(time=tim) # required for seasonal calculations
-            tmp_var = tmp_var.isel(time=slice(1,len(ds.time)-1))
-            #tmp_var = tmp_var.resample(time='QS-DEC').mean('time')
-            #tmp_var = tmp_var.chunk({'start_year':-1, 'time':-1, 'j':25, 'i':25})
+        tmp_var = tmp_var.assign_coords(time=tim) # required for seasonal calculations
+        tmp_var = tmp_var.isel(time=slice(1,len(ds.time)-1))
+        #tmp_var = tmp_var.resample(time='QS-DEC').mean('time')
+        #tmp_var = tmp_var.chunk({'start_year':-1, 'time':-1, 'j':25, 'i':25})
+            
+        ds_season = delayed(resample_season)(tmp_var)
 
-            corr = delayed(corr_NAO)(NAO.isel(r=r), tmp_var, dim='start_year')
-            corr = corr.compute()
+        corr = delayed(corr_NAO)(NAO.isel(r=r), ds_season, dim='start_year')
+            
+        #with ProgressBar():
+        corr = corr.compute()
 
-            # Save data
-            corr_save = xr.Dataset()
+        # Save data
+        corr_save = xr.Dataset()
 
-            corr_save[var] = corr
-            corr_save[var].attrs['standard_name'] = ["Corelations with seasonal mean NAO indices"]
-            save_file = save_path + "Correlation_NAO_" + var + "_r" + str(r+1) + ".nc"
-            corr_save.to_netcdf(save_file)
+        corr_save[var] = corr
+        corr_save[var].attrs['standard_name'] = ["Corelations with seasonal mean NAO indices"]
+        save_file = save_path + "Correlation_NAO_" + var + "_r" + str(r+1) + ".nc"
+        corr_save.to_netcdf(save_file)
 
-            print("Correlation data saved succefully")
+        print("Correlation data saved succefully")
--- Python_Scripts/Drift_ocean_derived_var_DCPP2016.py
+++ Python_Scripts/Drift_ocean_derived_var_DCPP2016.py
@@ -13,121 +13,125 @@ from dask.diagnostics import ProgressBar
 import warnings
 warnings.filterwarnings('ignore')
 
-#from dask_mpi import initialize
+from dask_mpi import initialize
 
 #cluster = SLURMCluster(cores=8,memory="16GB")
 #cluster.adapt(minimum=0, maximum=8)
 #client = Client(cluster)
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
     
-    #cluster = LocalCluster()
+#cluster = LocalCluster()
     
-    #initialize()
-    #client = Client()
-    #print(client)
+initialize()
+client = Client()
+#print(client)
 
-    ### ------ Function to compute potential density and sum over selected years ----------
+### ------ Function to compute potential density and sum over selected years ----------
 
-    def pdens(S,theta):
+def pdens(S,theta):
 
-        pot_dens = gsw.density.sigma0(S, theta)
+    pot_dens = gsw.density.sigma0(S, theta)
 
-        return pot_dens
+    return pot_dens
 
-    def processDataset(S, T, year1, year2, lead_year):
+def processDataset(S, T, year1, year2, lead_year):
 
-        ds_save = []
+    ds_save = []
 
-        for year in range(year1, year2):
+    for year in range(year1, year2):
 
-            # Extract data relavant start year and sum over all hindcasts
+        # Extract data relavant start year and sum over all hindcasts
             
-            #ds2 = ds1.sel(start_year = year - lead_year).isel(time=slice(12*lead_year, np.minimum(12 + 12*lead_year, len(ds1['time']))))
+        #ds2 = ds1.sel(start_year = year - lead_year).isel(time=slice(12*lead_year, np.minimum(12 + 12*lead_year, len(ds1['time']))))
             
-            tmp_S = S.sel(start_year = year - lead_year).isel(time=slice(12*lead_year, np.minimum(12 + 12*lead_year, len(S['time']))))
-            tmp_theta = T.sel(start_year = year - lead_year).isel(time=slice(12*lead_year, np.minimum(12 + 12*lead_year, len(T['time']))))
+        tmp_S = S.sel(start_year = year - 
+                      lead_year).isel(time=slice(12*lead_year, 
+                                                 np.minimum(12 + 12*lead_year, len(S['time']))))
+        tmp_theta = T.sel(start_year = year - 
+                          lead_year).isel(time=slice(12*lead_year, 
+                                                     np.minimum(12 + 12*lead_year, len(T['time']))))
             
-            ds2 = pdens(tmp_S, tmp_theta)
+        ds2 = pdens(tmp_S, tmp_theta)
 
-            ds_save.append(ds2)
+        ds_save.append(ds2)
 
-        return ds_save
+    return ds_save
 
-    ### ------------- Main computations ------------
+### ------------- Main computations ------------
 
-    ppdir="/badc/cmip6/data/CMIP6/DCPP/MOHC/HadGEM3-GC31-MM/dcppA-hindcast/"
+ppdir="/badc/cmip6/data/CMIP6/DCPP/MOHC/HadGEM3-GC31-MM/dcppA-hindcast/"
 
-    # for monthly drift
-    save_path="/home/users/hkhatri/DePreSys4_Data/Data_Drift_Removal/Drift_1970_2016_Method_DCPP/"
+# for monthly drift
+save_path="/home/users/hkhatri/DePreSys4_Data/Data_Drift_Removal/Drift_1970_2016_Method_DCPP/"
 
-    var_list = ['sigma0_surface'] 
+var_list = ['sigma0_surface'] 
 
-    year1, year2 = (1979, 2017) # range over to compute average using DCPP 2016 paper
+year1, year2 = (1979, 2017) # range over to compute average using DCPP 2016 paper
 
-    for var in var_list:
+for var in var_list:
 
-        for r in range(0,10):
+    for r in range(1,2):
 
-            print("Var = ", var, "; Ensemble = ", r)
+        print("Var = ", var, "; Ensemble = ", r)
 
-            ds = []
+        ds = []
 
-            for year in range(year1-10, year2, 1):
+        for year in range(year1-10, year2, 1):
 
-                # Read T/S data for each hindcast for every ensemble member
+            # Read T/S data for each hindcast for every ensemble member
 
-                var_path = "s" + str(year) +"-r" + str(r+1) + "i1p1f2/Omon/" + "tos" + "/gn/files/d20200417/"
-                d1 = xr.open_mfdataset(ppdir + var_path + "*.nc")
+            var_path = "s" + str(year) +"-r" + str(r+1) + "i1p1f2/Omon/" + "tos" + "/gn/latest/"
+            d1 = xr.open_mfdataset(ppdir + var_path + "*.nc")
 
-                var_path = "s" + str(year) +"-r" + str(r+1) + "i1p1f2/Omon/" + "sos" + "/gn/files/d20200417/" 
-                d2 = xr.open_mfdataset(ppdir + var_path + "*.nc")
+            var_path = "s" + str(year) +"-r" + str(r+1) + "i1p1f2/Omon/" + "sos" + "/gn/latest/" 
+            d2 = xr.open_mfdataset(ppdir + var_path + "*.nc")
 
-                d = xr.merge([d1.tos, d2.sos])
+            d = xr.merge([d1.tos, d2.sos])
 
-                d = d.drop('time') # drop time coordinate as different time values create an issue in concat operation
+            d = d.drop('time') # drop time coordinate as different time values create an issue in concat operation
 
-                ds.append(d)
+            ds.append(d)
 
-            # combine data for hindcasts
-            ds = xr.concat(ds, dim='start_year')
-            #ds = ds.drop(['vertices_latitude', 'vertices_longitude', 'time_bnds'])
-            ds = ds.isel(i=slice(749,1199), j = slice(699, 1149))
+        # combine data for hindcasts
+        ds = xr.concat(ds, dim='start_year')
+        ds = ds.isel(i=slice(749,1199), j = slice(699, 1149))
 
-            ds = ds.assign(start_year = np.arange(year1-10, year2, 1))
-            ds = ds.chunk({'start_year':-1, 'time':-1, 'j':25, 'i':25})
+        ds = ds.assign(start_year = np.arange(year1-10, year2, 1))
+        ds = ds.chunk({'start_year':-1, 'time':-1, 'j':25, 'i':25})
 
-            print("Data read complete")
+        print("Data read complete")
 
-            #rho = xr.apply_ufunc(pdens, ds.sos, ds.tos, dask='parallelized',
-            #        output_dtypes=[ds.tos.dtype])
-            #rho = pdens(ds.sos, ds.tos)
-            #rho = delayed(pdens)(ds.sos, ds.tos)
+        #rho = xr.apply_ufunc(pdens, ds.sos, ds.tos, dask='parallelized',
+        #        output_dtypes=[ds.tos.dtype])
+        #rho = pdens(ds.sos, ds.tos)
+        #rho = delayed(pdens)(ds.sos, ds.tos)
 
-            # loop over lead year and compute mean values
-            for lead_year in range (0,11):
+        # loop over lead year and compute mean values
+        for lead_year in range (0,11):
 
-                #print("Lead Year running = ", lead_year) 
+            #print("Lead Year running = ", lead_year) 
 
-                #sigma0 = processDataset(rho, year1, year2, lead_year)
-                #sigma0 = delayed(processDataset)(rho, year1, year2, lead_year)
-                sigma0 = processDataset(ds.sos, ds.tos, year1, year2, lead_year)
+            #sigma0 = processDataset(rho, year1, year2, lead_year)
+            #sigma0 = delayed(processDataset)(rho, year1, year2, lead_year)
+            sigma0 = processDataset(ds.sos, ds.tos, year1, year2, lead_year)
 
-                sigma0 = sum(sigma0) / (year2 - year1)
-                #sigma0 = delayed(sum)(sigma0) / (year2 - year1)
+            sigma0 = sum(sigma0) / (year2 - year1)
+            #sigma0 = delayed(sum)(sigma0) / (year2 - year1)
                 
-                with ProgressBar():
-                    sigma0 = sigma0.compute() #scheduler="processes")
-                #sigma0 = compute(sigma0) #, scheduler="processes")
+            #with ProgressBar():
+            #    sigma0 = sigma0.compute() #scheduler="processes")
+            sigma0 = sigma0.compute() #, scheduler="processes")
 
-                ds_save = xr.Dataset()
-                ds_save['sigma0'] = sigma0
-                ds_save.sigma0.attrs['standard_name'] = "Potential Density wrt zero pressure - 1000"
-                ds_save.sigma0.attrs['units'] = "kg/m3"
+            ds_save = xr.Dataset()
+            ds_save['sigma0'] = sigma0
+            ds_save.sigma0.attrs['standard_name'] = "Potential Density wrt zero pressure - 1000"
+            ds_save.sigma0.attrs['units'] = "kg/m3"
 
-                save_file = save_path +"Drift_" + var + "_r" + str(r+1)+ "_Lead_Year_" + str(int(lead_year+1)) + ".nc"
-                ds_save.to_netcdf(save_file)
+            save_file = (save_path +"Drift_" + var + "_r" + str(r+1)+ "_Lead_Year_" +
+                         str(int(lead_year+1)) + ".nc")
+            ds_save.to_netcdf(save_file)
 
-                print("File saved for Lead Year = ", lead_year + 1)
+            print("File saved for Lead Year = ", lead_year + 1)
 
-#client.close()
+client.close()
