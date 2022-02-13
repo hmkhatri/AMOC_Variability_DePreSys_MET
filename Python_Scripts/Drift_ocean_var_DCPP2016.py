16055be52ed32e2b9130a8ba9dbe7fda76c1b4e5
Hemant Khatri <hemantkhatri1091@gmail.com>
2022-02-13 12:15:29 +0000

    update script for computing drift for 3D vars

--- Python_Scripts/Drift_ocean_var_DCPP2016.py
+++ Python_Scripts/Drift_ocean_var_DCPP2016.py
@@ -3,13 +3,21 @@ import numpy as np
 import scipy as sc
 import xarray as xr
 import dask.distributed
-from dask.distributed import Client
+from dask.distributed import Client, performance_report
 from dask import delayed
 from dask import compute
 from dask import persist
 
 import warnings
 warnings.filterwarnings('ignore')
+
+from dask_mpi import initialize
+initialize()
+client = Client()
+
+import dask
+import distributed
+dask.config.set({"distributed.comm.timeouts.tcp": "50s"})
 
 ### ------ Function to sum over selected years ----------
 
@@ -37,16 +45,18 @@ ppdir="/badc/cmip6/data/CMIP6/DCPP/MOHC/HadGEM3-GC31-MM/dcppA-hindcast/"
 #save_path="/home/users/hkhatri/DePreSys4_Data/Data_Drift_Removal/Drift_2016_DCPP/"
 
 # for monthly drift
-save_path="/home/users/hkhatri/DePreSys4_Data/Data_Drift_Removal/Drift_1970_2016_Method_DCPP/"
+#save_path="/home/users/hkhatri/DePreSys4_Data/Data_Drift_Removal/Drift_1970_2016_Method_DCPP/"
+
+save_path="/gws/nopw/j04/snapdragon/hkhatri/Data_Drift/" # this is for 3D vars
 
 #var_list = ['hfds', 'tos', 'sos'] #, 'mlotst', 'zos']
-var_list = ['mlotst']
+var_list = ['thetao']
 
 year1, year2 = (1979, 2017) # range over to compute average using DCPP 2016 paper
 
 for var in var_list:
     
-    for r in range(0,10):
+    for r in range(1,10):
        
         print("Var = ", var, "; Ensemble = ", r)
 
@@ -58,18 +68,29 @@ for var in var_list:
             
             var_path = "s" + str(year) +"-r" + str(r+1) + "i1p1f2/Omon/" + var + "/gn/latest/"
             
-            d = xr.open_mfdataset(ppdir + var_path + "*.nc")
-            d = d.drop('time') # drop time coordinate as different time values create an issue in concat operation
+            d = xr.open_mfdataset(ppdir + var_path + "*.nc", parallel=True, decode_times=False)
+            
+            # drop time coordinate as different time values create an issue in concat operation
+            d = d.drop(['time', 'vertices_latitude', 'vertices_longitude', 'time_bnds'])
+            if(var=='thetao'):
+                d = d.drop('lev_bnds')
+                
+            d = d.isel(i=slice(749,1199), j = slice(699, 1149))
             
             ds.append(d)
             
         # combine data for hindcasts
         ds = xr.concat(ds, dim='start_year')
-        ds = ds.drop(['vertices_latitude', 'vertices_longitude', 'time_bnds'])
-        ds = ds.isel(i=slice(749,1199), j = slice(699, 1149))
+        #ds = ds.drop(['vertices_latitude', 'vertices_longitude', 'time_bnds'])
+        #ds = ds.isel(i=slice(749,1199), j = slice(699, 1149))
         
         ds = ds.assign(start_year = np.arange(year1-10, year2, 1))
-        ds = ds.chunk({'start_year':1, 'j':50, 'i':50})
+        #ds = ds.chunk({'start_year':1, 'j':50, 'i':50})
+        
+        #if(var=='thetao'):
+        #    ds = ds.chunk({'start_year':1, 'time':12, 'lev':5})
+        #else:
+        #    ds = ds.chunk({'start_year':1, 'time':12})
         
         print("Data read complete")
         
@@ -78,13 +99,19 @@ for var in var_list:
     
             #print("Lead Year running = ", lead_year)
 
-            ds_save = processDataset(ds, year1, year2, lead_year)
+            ds_save = xr.map_blocks(processDataset, ds, year1, year2, lead_year)
     
             ds_save = sum(ds_save) / (year2 - year1)
 
+            #with performance_report(filename="dask-report.html"):
             ds_save = ds_save.compute()
     
-            save_file = save_path +"Drift_" + var + "_r" + str(r+1)+ "_Lead_Year_" + str(int(lead_year+1)) + ".nc"
+            save_file = (save_path +"Drift_" + var + "_r" + str(r+1)+ 
+                         "_Lead_Year_" + str(int(lead_year+1)) + ".nc")
             ds_save.to_netcdf(save_file)
 
             print("File saved")
+            
+        ds.close()
+
+client.close()
