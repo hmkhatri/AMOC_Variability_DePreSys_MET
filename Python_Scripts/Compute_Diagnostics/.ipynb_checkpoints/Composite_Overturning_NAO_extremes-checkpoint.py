91f48b0c953bccb3b2057bfe49b38b30231694fe
Hemant Khatri <hemantkhatri1091@gmail.com>
2022-07-25 00:12:38 +0100

    Composite and confidence intervals with barotropic-flow-corrected overturning, heat transport diagnostics

--- Python_Scripts/Compute_Diagnostics/.ipynb_checkpoints/Composite_Overturning_NAO_extremes-checkpoint.py
+++ Python_Scripts/Compute_Diagnostics/.ipynb_checkpoints/Composite_Overturning_NAO_extremes-checkpoint.py
@@ -46,7 +46,7 @@ NAO_season = NAO.resample(time='QS-DEC').mean('time')
 # NAO_cut = 2.5 # based on plot for individual normalised NAO values
 NAO_cut = 1300. # based on plot for individual NAO values in pa
 
-case = 'NAOp' 
+case_list = ['NAOp', 'NAOn'] 
 #case = 'NAOn'
 
 # read drift model data
@@ -77,53 +77,56 @@ ds_drift = ds_drift.chunk({'time':1})
 
 print("Reading Drift Data Complete")
 
-ds = []
-
-for tim_ind in range(4,13,4):
+for case in case_list:
     
-    ind_NAOp = xr.where(NAO_season.isel(time=tim_ind) >= NAO_cut, 1, 0)
-    ind_NAOn = xr.where(NAO_season.isel(time=tim_ind) <= -NAO_cut, 1, 0)
-    
-    if (case == 'NAOp'):
-        count_NAO = ind_NAOp
-    elif (case == 'NAOn'):
-        count_NAO = ind_NAOn
-    else:
-        print("Choose a valid case")
+    ds = []
 
-    for r in range(0,10):
+    for tim_ind in range(4,13,4):
 
-        d = xr.open_dataset(data_dir + "Overturning_Heat_Transport_r" + str(r+1) + ".nc", chunks={'start_year':1})
-        
-        for year in range(year1, year2, 1):
-            
-            if(count_NAO.isel(r=r).sel(start_year=year) == 1):
-                
-                ds1 = d.sel(start_year=year).drop(['start_year']) - ds_drift.isel(r=r).drop(['Overturning_max_z', 'Overturning_max_sigma'])
-                
-                # compute anomaly in overturning maximum
-                ds1['Overturning_max_z'] = ((d['Overturning_z'].sel(start_year=year).drop(['start_year'])).max(dim='lev') - 
-                                            (ds_drift['Overturning_max_z'].isel(r=r)))
-                
-                ds1['Overturning_max_sigma'] = ((d['Overturning_sigma'].sel(start_year=year).drop(['start_year'])).max(dim='sigma0') -
-                                                (ds_drift['Overturning_max_sigma'].isel(r=r)))
-                
-                # to get mean densities and layer thicknesses
-                ds1['Depth_sigma'] = d['Depth_sigma'].sel(start_year=year).drop(['start_year'])
-                ds1['Density_z'] = d['Density_z'].sel(start_year=year).drop(['start_year'])
-                
-                ds.append(ds1.isel(time = slice((int(tim_ind/4)-1)*12, (int(tim_ind/4) + 7)*12 + 5)))
+        ind_NAOp = xr.where(NAO_season.isel(time=tim_ind) >= NAO_cut, 1, 0)
+        ind_NAOn = xr.where(NAO_season.isel(time=tim_ind) <= -NAO_cut, 1, 0)
 
-ds = xr.concat(ds, dim='comp')
+        if (case == 'NAOp'):
+            count_NAO = ind_NAOp
+        elif (case == 'NAOn'):
+            count_NAO = ind_NAOn
+        else:
+            print("Choose a valid case")
 
-ds = ds.drop('latitude')
-ds['latitude'] = ds_drift['latitude'].isel(r=0, time=0) # get actual latitude values for overturning
-    
-print("Composite Data read complete")
-print("Total cases = ", len(ds['comp']), " - case ", case)
+        for r in range(0,10):
 
-comp_save = ds.astype(np.float32).compute()
+            d = xr.open_dataset(data_dir + "Overturning_Heat_Salt_Transport_r" + str(r+1) + ".nc", chunks={'start_year':1})
 
-save_file = save_path + "Composite_" + case + "_Overturning.nc"
-comp_save.to_netcdf(save_file)
-print("Data saved successfully")
+            for year in range(year1, year2, 1):
+
+                if(count_NAO.isel(r=r).sel(start_year=year) == 1):
+
+                    ds1 = (d.sel(start_year=year).drop(['start_year']) - 
+                           ds_drift.isel(r=r).drop(['Overturning_max_z', 'Overturning_max_sigma']))
+
+                    # compute anomaly in overturning maximum
+                    ds1['Overturning_max_z'] = ((d['Overturning_z'].sel(start_year=year).drop(['start_year'])).max(dim='lev') - 
+                                                (ds_drift['Overturning_max_z'].isel(r=r)))
+
+                    ds1['Overturning_max_sigma'] = ((d['Overturning_sigma'].sel(start_year=year).drop(['start_year'])).max(dim='sigma0') -
+                                                    (ds_drift['Overturning_max_sigma'].isel(r=r)))
+
+                    # to get mean densities and layer thicknesses
+                    ds1['Depth_sigma'] = d['Depth_sigma'].sel(start_year=year).drop(['start_year'])
+                    ds1['Density_z'] = d['Density_z'].sel(start_year=year).drop(['start_year'])
+
+                    ds.append(ds1.isel(time = slice((int(tim_ind/4)-1)*12, (int(tim_ind/4) + 7)*12 + 5)))
+
+    ds = xr.concat(ds, dim='comp')
+
+    ds = ds.drop('latitude')
+    ds['latitude'] = ds_drift['latitude'].isel(r=0, time=0) # get actual latitude values for overturning
+
+    print("Composite Data read complete")
+    print("Total cases = ", len(ds['comp']), " - case ", case)
+
+    comp_save = ds.astype(np.float32).compute()
+
+    save_file = save_path + "Composite_" + case + "_Overturning_MHT.nc"
+    comp_save.to_netcdf(save_file)
+    print("Data saved successfully")
